4deac274b2c29d900bb26712ca68fc2763a2156c
fyardlest <fyardlest1@gmail.com>
2021-07-19 19:29:33 -0400

    reverse string js code OK

--- js/site.js
+++ js/site.js
@@ -1,21 +1,44 @@
-// Get the value from the interface.
-// Start or Controller function or Entry point.
-const getValues = () => {
-    // get value from page.
+// Get the string from the user.
+// Our Controller function or Entry point.
+const getValue = () => {
+    document.getElementById('theAlert').classList.add('invisible')
 
-    // we call generatenumbers
-
-    // we call displayNumbers
+	// get string from page.
+    let userString = document.getElementById('userString').value
+	// we call reverseString
+	let revString = reverseString(userString)
+	// we call displayReverseString
+	displayReverseString(revString)
 }
 
-// Generate numbers from the start value to the end value.
+// Reverse the string.
 // Our logic function(s)
-const generatenumbers = (start, end) => {
-    //
+const reverseString = (userString) => {
+    let reverseString = []
+
+    // let name = 'Yardley'
+    // name[0] = 'Y'
+    // name[6] = 'y'
+    // name.length = 7
+    // the last position in the name array = name.length - 1
+    
+    // reverse a string with a for loop
+    for (
+		let index = userString.length - 1;
+        index >= 0;
+		index--
+	) {
+		reverseString += userString[index]
+    }
+    
+    return reverseString
 }
 
-// Display the even numbers in bold.
+// Display the reversed string to the user.
 // Display or view functions
-const displayNumbers = () => {
-    //
+const displayReverseString = (revString) => {
+	// write a messase of the page
+    document.getElementById('msg').innerHTML = `Your string reversed is: ${revString}`
+    // show the alert box
+    document.getElementById('theAlert').classList.remove('invisible')
 }
